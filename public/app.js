a7a99a04a62403ed657526a9df78598ea237db8a
Isaac Sterling <isaacjeffrey789@gmail.com>
2022-02-08 17:40:22 -0800

    Undoing attempts to run backend in-browser
    Maybe use create-react-app? Was conflating browser js scripts with a react app

--- public/app.js
+++ public/app.js
@@ -1,11 +1,10 @@
-import { initializeApp } from 'firebase/app';
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
-var firebase = require("firebase/app");
+import firebase from "firebase/app";
 
 // Add the Firebase products that you want to use
-require("firebase/auth");
-require("firebase/firestore");
+import "firebase/auth";
+import "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
@@ -20,3 +19,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+console.log(firebase);
