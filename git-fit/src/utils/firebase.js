19f720fa4a30c89ff14838a21c32f8d44d3875a4
Isaac Sterling <isaacjeffrey789@gmail.com>
2022-02-19 18:27:44 -0800

    addUser function added
    Not stable build -- has issues locating firestore module

--- git-fit/src/utils/firebase.js
+++ git-fit/src/utils/firebase.js
@@ -1,8 +1,7 @@
 import firebase from "firebase"
-import "firebase/firestore"
+import auth, { createUserWithEmailAndPassword, getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-
     apiKey: "AIzaSyC6FPv049UYKEjiTvEM2yxQaFKVyqgMNdA",
     authDomain: "git-fit-26023.firebaseapp.com",
     projectId: "git-fit-26023",
@@ -11,6 +10,19 @@ const firebaseConfig = {
     appId: "1:778817305393:web:70156ce96b0be98996ce10",
     measurementId: "G-42JLXMNTKW"
 };
+
+
+export const addUser = function(email, password){
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+        })
+        .catch(() => {
+            //const errorCode = error.code;
+            //const errorMessage = error.message;
+        });
+}
 
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
