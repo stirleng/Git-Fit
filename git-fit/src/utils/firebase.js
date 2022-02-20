36b4180d4dcc58228cadc2a1c0d3cd5eb36fc115
Ethan Truong <18603664+2Retr0@users.noreply.github.com>
2022-02-19 18:37:33 -0800

    Revert "Merge branch 'main' of https://github.com/stirleng/Git-Fit"
    This reverts commit 03f4265841d4c0977bd70a1acd64602e12689349, reversing
    changes made to 5c0e0c2403e29a6cb7bd3f28aaafc4a32c2ebe6e.

--- git-fit/src/utils/firebase.js
+++ git-fit/src/utils/firebase.js
@@ -1,7 +1,8 @@
 import firebase from "firebase"
-import auth, { createUserWithEmailAndPassword, getAuth } from "firebase/auth"
+import "firebase/firestore"
 
 const firebaseConfig = {
+
     apiKey: "AIzaSyC6FPv049UYKEjiTvEM2yxQaFKVyqgMNdA",
     authDomain: "git-fit-26023.firebaseapp.com",
     projectId: "git-fit-26023",
@@ -10,19 +11,6 @@ const firebaseConfig = {
     appId: "1:778817305393:web:70156ce96b0be98996ce10",
     measurementId: "G-42JLXMNTKW"
 };
-
-
-export const addUser = function(email, password){
-    const auth = getAuth();
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-        })
-        .catch(() => {
-            //const errorCode = error.code;
-            //const errorMessage = error.message;
-        });
-}
 
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
