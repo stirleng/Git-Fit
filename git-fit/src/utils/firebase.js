3f2fb932859b406e1dd8f48f1e0ddf0e48933826
Isaac Sterling <isaacjeffrey789@gmail.com>
2022-02-19 17:37:52 -0800

    firebase.js added
    Firebase set up, imported to index.js

---
+++ git-fit/src/utils/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyC6FPv049UYKEjiTvEM2yxQaFKVyqgMNdA",
+    authDomain: "git-fit-26023.firebaseapp.com",
+    projectId: "git-fit-26023",
+    storageBucket: "git-fit-26023.appspot.com",
+    messagingSenderId: "778817305393",
+    appId: "1:778817305393:web:70156ce96b0be98996ce10",
+    measurementId: "G-42JLXMNTKW"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
