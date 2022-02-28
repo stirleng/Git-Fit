c01180b3d557e2c8a7f3f1a84c4d48a093317ed0
2Retr0 <18603664+2Retr0@users.noreply.github.com>
2022-02-27 22:16:08 -0800

    Removed deprecated code.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import React from 'react';
-
 import App from './App';
 import './index.css'
-import './components/Screens/signup.css'
-
 import * as ReactDOM from 'react-dom';
 
-
-ReactDOM.render( // We use this wrapper format to have App render on top of the background element.
-    // <div className='wrapper'>
-    //     <div className='bottom-layer'>
-    //         <div className='background'/>
-    //     </div>
-    //     <div className='top-layer'>
-    //         <App/>
-    //     </div>
-    // </div>
-  
-        <App/>
-
-    
-    , document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
