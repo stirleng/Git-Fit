0ab958180974fa78d30484ae1166a19ea709b235
Jack Lin <jacklin1229@gmail.com>
2022-02-28 01:02:37 -0800

    cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -8,14 +8,6 @@ import * as ReactDOM from 'react-dom';
 
 
 ReactDOM.render( // We use this wrapper format to have App render on top of the background element.
-    // <div className='wrapper'>
-    //     <div className='bottom-layer'>
-    //         <div className='background'/>
-    //     </div>
-    //     <div className='top-layer'>
-    //         <App/>
-    //     </div>
-    // </div>
   
         <App/>
 
