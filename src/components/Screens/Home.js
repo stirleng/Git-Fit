e510c5d342384e082482e8bd270068d568b20afd
AuracleDelphi <AuracleDelphi@users.noreply.github.com>
2022-02-27 22:55:11 -0800

    Added button functionality to home screen
    - Buttons are now clickable
    - Buttons link to placeholder screens

--- src/components/Screens/Home.js
+++ src/components/Screens/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { useAuth } from '../../contexts/AuthContext'
 import './Home.css'
+import { Link, useNavigate, useParams, useLocation} from 'react-router-dom';
+// How to use react-router-dom:
+// https://github.com/remix-run/react-router/blob/main/docs/getting-started/tutorial.md
+//import Button from 'react-bootstrap/Button'
 
-export default function Home() {
-
+export default function Home(props) {
+  let navigate = useNavigate();
+  
   const {currentUser} = useAuth();
   console.log(currentUser);
   return (
@@ -12,12 +17,16 @@ export default function Home() {
         Home Page
       </div>
       <div className='HomePageButtons'>
-        <div className='WorkoutsPageButton'>
-          Workout Button
-        </div>
-        <div className='MealsPageButton'>
-          Meals Button
-        </div>
+        <Link to="/workouts">
+          <button type="button" className='WorkoutsPageButton'>
+                Workout Button
+          </button>
+        </Link>
+        <Link to="/meals">
+          <button type="button" className='MealsPageButton'>
+                Meals Button
+          </button>
+        </Link>
       </div>
     </div>
   )
