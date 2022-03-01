efc74a700742fafbcde13c2da2e4dea9b5e2ea54
stirleng <isaacjeffrey789@gmail.com>
2022-02-28 23:12:10 -0800

    Home to Workout Button Styling
    Added button transition effect, changed button fonts and border color, used heavier header font, shortened header background color.

--- src/components/Screens/Home.js
+++ src/components/Screens/Home.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { useAuth } from '../../contexts/AuthContext'
 import './Home.css'
 import { Link, useNavigate, useParams, useLocation} from 'react-router-dom';
-import HomePageImage from '../images/HomePageImage.jpg'
+import '../images/HomePageBackground.jpg'
+import '../images/BarbellImage.png'
 // How to use react-router-dom:
 // https://github.com/remix-run/react-router/blob/main/docs/getting-started/tutorial.md
 //import Button from 'react-bootstrap/Button'
@@ -18,15 +19,20 @@ export default function Home(props) {
       <div className='HomeScreenHeader'>
         Git-Fit
       </div>
-      <div className='HomePageButtons'>
+      <div className='HomePageButtonContainer'>
         <Link to="/workouts">
-          <button type="button" className='WorkoutsPageButton'>
-                Workout Button
+          <button type="button" className='HomePageButtons WorkoutsPageButton'>
+                Your Workouts
+                <br></br>
+                <img src={require('../images/BarbellImage.png')} className='BarbellImage'/>
+                <div className='NavFromHomeArrow'>
+                 &#8594;
+                </div>
           </button>
         </Link>
         <Link to="/meals">
-          <button type="button" className='MealsPageButton'>
-                Meals Button
+          <button type="button" className='HomePageButtons MealsPageButton'>
+                Your Meals
           </button>
         </Link>
       </div>
