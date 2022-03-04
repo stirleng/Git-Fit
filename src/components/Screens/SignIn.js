62168465b3d71e1b6cdca8c7d25073877b6e9000
2Retr0 <18603664+2Retr0@users.noreply.github.com>
2022-03-04 11:47:14 -0800

    Update Sign In page visuals.

--- src/components/Screens/SignIn.js
+++ src/components/Screens/SignIn.js
@@ -1,21 +1,31 @@
 import {useAuth} from '../../contexts/AuthContext';
-import React, {useState} from 'react';
-import {auth} from '../../firebase.js';
+import React, {useEffect, useRef, useState} from 'react';
 import EmailIcon from '@mui/icons-material/Email';
 import VpnKeyIcon from '@mui/icons-material/VpnKey';
 import TextBox from "../ui/TextBox";
 import MyButton from "../ui/Button"
-import './signin.css'
-import {Link, useNavigate} from 'react-router-dom';
+import classes from './SignIn.module.css'
+import './Home.css'
+import {Link, useNavigate, useLocation} from 'react-router-dom';
 
 function SignIn() {
-    const commonProps = {className: 'inputBox', textColor: 'white', bgColor: 'black', width: 275, height: 40};
+    const commonProps = {className: classes.inputBox, textColor: 'black', bgColor: 'white', width: 375, height: 40};
+    const buttonProps = {className: classes.inputBox, textColor: 'white', bgColor: '#e0b100', width: 375, height: 50};
+
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
     const {signIn} = useAuth();
     let navigate = useNavigate();
+
+    useEffect(() => {
+        document.body.setAttribute('style',
+            `position: fixed; top: -$(this.windowOffset)px; left: 0; right: 0;`);
+
+        return () => {
+            document.body.setAttribute('style', '');
+        };
+    }, []);
 
     async function handleSubmit(e) {
         e.preventDefault();
@@ -36,12 +46,34 @@ function SignIn() {
         return navigate('/');
     }
 
-    return (<div className='wrapper'>
-        <div id='bottom-layer' className='signInBackground'/>
-        <div id='top-layer' className='container'>
-            <form onSubmit={handleSubmit}>
-                <h1>Sign In Page</h1>
-                <div id='inputFieldContainer'>
+    return (<div className={classes.wrapper}>
+        <div id='bottom-layer' className={classes.signInBackground}/>
+        <div id='top-layer' className={classes.slide}
+            style={{background: '#e0b100',
+                transform: 'rotate(7deg) translate(-12%, -5%)'}}/>
+
+        <div id='top-layer' className={classes.slide}/>
+
+        <div id='top-layer' className={classes.rowContainer}>
+        <div className={`${classes.logo} ${classes.card}`}
+             style={{padding: '0 9px 0 5px',}}>
+            Git-Fit
+        </div>
+        <h2 style={{padding: '0 30px 0 30px'}}>
+            Sign In
+        </h2>
+            <h2 style={{paddingRight: '30px'}}>
+                Sign Up
+            </h2>
+            <h2 style={{paddingRight: '30px'}}>
+                About
+            </h2>
+        </div>
+
+        <div id='top-layer' className={classes.container}>
+            <div className={classes.card}>
+                <form onSubmit={handleSubmit}>
+                    <h1>Welcome back!</h1>
                     <TextBox {...commonProps}
                              placeholder={'Email'}
                              adornment={<EmailIcon/>}
@@ -51,12 +83,12 @@ function SignIn() {
                              placeholder={'Password'}
                              adornment={<VpnKeyIcon/>}
                              handler={e => setPassword(e.target.value)}/>
-                    <MyButton {...commonProps}
+                    <MyButton {...buttonProps}
                               type='submit'
-                              value={'Sign In!'}/>
-                </div>
-                Need an account? <Link to="/signup">Sign Up</Link>
-            </form>
+                              value={'SIGN IN!'}/>
+                    Need an account? <Link to="/signup">Sign Up</Link>
+                </form>
+            </div>
         </div>
     </div>);
 }
