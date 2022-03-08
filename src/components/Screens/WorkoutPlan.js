882229961d12e623fa8ad63c83f793ab54823003
lzaydes <98509255+lzaydes@users.noreply.github.com>
2022-03-07 17:17:57 -0800

    Access documents as objects upon selection by user via drop-down menu

--- src/components/Screens/WorkoutPlan.js
+++ src/components/Screens/WorkoutPlan.js
@@ -9,27 +9,48 @@ import {  collection, getDocs, query, where, limit } from 'firebase/firestore'
 export default function WorkoutPlan (){
 
     const {setPlan} = useAuth()
-const [pref, setPref] = useState('')
-async function handleClick(e){
-    const plan =  query(collection(db, "workout"), where("Category", "==", {pref}), limit(6));
+    const [pref, setPref] = useState('')
+    async function handleClick(e){
+
+    const workouts = collection(db, "workout")
+
+    e.preventDefault()
+
+        if (pref == ''){
+            alert("Please select a category")
+            return
+        }
+
+    const plan =  query(workouts, where("Category", "==", pref), limit(6));
+    const getPlan = await getDocs(plan)
+
+    getPlan.forEach((doc) => {console.log(doc.data())})
+    
+
 }
 
 
     return (
-        <body>
+        <div id = "body">
              <h1> What do you want to workout today?</h1>
-                <div id='inputFieldContainer'>
-                  <input 
-                    className='inputBox'
-                    placeholder='  Preference'
-                    type="text"
-                    value={pref}
-                    onChange={(e) =>{
-                        setPref(e.target.value)
-                        }
-                    }
-                  />
+            <div id = "select">
+                <select onClick={(e) => {setPref(e.target.value)}}>
+                    Workout Preference
+                    <option value=''>Select a Category</option>
+                    <option value = "chest">Chest</option>
+                    <option value="back">Back</option>
+                    <option value="leg">Legs</option>
+                    <option value="bicep">Bicep</option>
+                    <option value="shoulder">Shoulders</option>
+                    <option value="tricep">Triceps</option>
+                </select>
+             <div>
+                    <button id = "submit" type='button'
+                    onClick={(e) => {handleClick(e)}}>
+                    Get your workout    
+                </button>
+            </div>
+         </div>
              </div>
-        </body>
     )
 }
