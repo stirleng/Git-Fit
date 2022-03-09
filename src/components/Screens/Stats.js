5caff2c5299066ace23296c32b5903320bca48b3
lzaydes <98509255+lzaydes@users.noreply.github.com>
2022-03-09 05:59:23 -0800

    Access user info for stats via backend
    userData arraywon't update, need to work on saving the data + styling the page and link button to it.

--- src/components/Screens/Stats.js
+++ src/components/Screens/Stats.js
@@ -1,32 +1,66 @@
 import React,  {useState, useEffect} from 'react'
+import firebase from 'firebase/compat/app';
 import { useAuth } from '../../contexts/AuthContext'
 import { db, collection, getDocs, query, where, limit, useFirestoreDocument, QuerySnapshot} from '../../firebase';
 import './Stats.css'
 import { Link, useNavigate, useParams, useLocation} from 'react-router-dom';
+import { doc, getDoc, DocumentSnapshot, FieldPath, documentId, forEach } from "firebase/firestore";
+
+ function getUserData(uid) {
+        firebase.database().ref('users/' + uid).once("Age", snap => {
+            console.log(snap.val())
+        })
+    }
 
 export default function Stats() {
     const {currentUser} = useAuth()
-    let age = currentUser.age
-    let sex = currentUser.sex
-    let weight = 0.453592 * currentUser.weight
-    let height = 2.54 * (12 * currentUser.feet + currentUser.inches)
+   // await db.collection("users").doc(currentUser.uid).get().then((snapshot) =>{
+        //access data
+    //})
+    let uid = currentUser.uid
+
+    let userData = []
+    
+await        async function onRender(){
+            await db.collection("users").doc(uid).get().then((snapshot) =>{
+                        userData.push(snapshot.get("Age"))
+                         userData.push(snapshot.get("Height_ft"))
+                       userData.push(snapshot.get("Height_in"))
+                        userData.push(snapshot.get("Weight"))
+                        //userData.push(snapshot.get("Sex"))
+                  console.log(userData)
+              })
+            }
+    
+            onRender()
+
+          console.log(userData)
+    let age = userData[0]//getUserData(uid)
+    let sex = 'm'//currentUser.sex
+    let weight = 0.453592 * userData[3]
+    let height = 2.54 * (12 * userData[1] + userData[2])
+
 
     let bmr = 0
     if (sex =='m')
     {
         bmr =  88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
-        console.log(bmr)
     }
 
     else{
         bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
     }
 
+    let projected = bmr - 500
+
     return(
         <div>
-        <h1>
-            yoyoyoy {currentUser.age}
-        </h1>
+            <div>
+                <button type="button" onClick={(e) => {}}>
+                    try
+                </button>
+            </div>
+            If you eat fewer than {age} calories a day, you will be lbs in 5 weeks!
         </div>
     )
 }
