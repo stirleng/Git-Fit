3cf6f0aa18572e0f61fb399a477db17315d2f337
AuracleDelphi <AuracleDelphi@users.noreply.github.com>
2022-03-09 03:29:56 -0800

    Update WorkoutSearch.js
    Added search code.
    Currently trying to sort out returning promises vs docs.

--- src/components/Screens/WorkoutSearch.js
+++ src/components/Screens/WorkoutSearch.js
@@ -5,63 +5,7 @@ import './UploadWorkout.css'
 // Added geofire for recording locations and searching nearby
 const geofire = require('geofire-common');
 
-// Geofire reference/instructions
-
-// queryHashes takes in lat, lng, dist, and returns docs for 
-// workouts within dist of [lat,lng]
-function queryHashes(lat=34, lng=-118, dist=10) {
-    //js/react goes here
-
-    // CODE from https://firebase.google.com/docs/firestore/solutions/geoqueries
-    // TODO: Edit code so it... actually works
-    // find workouts within x km of lat, lng
-    const center = [lat, lng] // TODO: replace placeholder values with user location
-    const radiusInM = dist*1000 // 100 km TODO: Replace with variable distance
-
-    // Each item in 'bounds' represents a startAt/endAt pair. We have to issue
-    // a separate query for each pair. There can be up to 9 pairs of bounds
-    // depending on overlap, but in most cases there are 4.
-    const bounds = geofire.geohashQueryBounds(center, radiusInM);
-    const promises = [];
-    for (const b of bounds) {
-        const q = db.collection('workout')
-            .orderBy('LocationHash')
-            .startAt(b[0])
-            .endAt(b[1]);
-        promises.push(q.get());
-    }
-
-    // Collect all the query results together into a single list
-    Promise.all(promises).then((snapshots) => {
-        const matchingDocs = [];
-
-        for (const snap of snapshots) {
-            for (const doc of snap.docs) {
-                const lat = doc.get('Latitude');
-                const lng = doc.get('Longitude');
-
-                // We have to filter out a few false positives due to GeoHash
-                // accuracy, but most will match
-                const distanceInKm = geofire.distanceBetween([parseInt(lat), parseInt(lng)], center);
-                const distanceInM = distanceInKm * 1000;
-                if (distanceInM <= radiusInM) {
-                    matchingDocs.push(doc);
-                }
-            }
-        }
-    
-        return matchingDocs;
-    }).then((matchingDocs) => {
-    // Process the matching documents
-        console.log("docs")
-        console.log(matchingDocs[0].get('Description'))
-    });
-}
-function search(type, intensity, calories, lat, lng, dist){
-    const workoutsRef = db.collection('workouts');
-}
-
-export default function UploadWorkout(props) {
+export default function WorkoutSearch(props) {
     const {setWorkout} = useAuth();
     
     const [error, setError] = useState("")
@@ -74,7 +18,77 @@ export default function UploadWorkout(props) {
     const [searchRadius, setSearchRadius] = useState(0)
     const [latitude, setLatitude] = useState(null)
     const [longitude, setLongitude] = useState(null)
-    //TODO: Make calories, link, intensity, description, fields optional
+    const [searchResults, setSearchResults] = useState([])
+
+    // queryHashes takes in lat, lng, dist, and returns docs for 
+    // workouts within dist of [lat,lng]
+    async function queryHashes(lat=34, lng=-118, dist=10) {
+        //js/react goes here
+
+        // CODE from https://firebase.google.com/docs/firestore/solutions/geoqueries
+        // TODO: Edit code so it... actually works
+        // find workouts within x km of lat, lng
+        const center = [parseInt(lat), parseInt(lng)] // TODO: replace placeholder values with user location
+        const radiusInM = parseInt(dist)*1000 // 100 km TODO: Replace with variable distance
+        dist = 20000000
+        // Each item in 'bounds' represents a startAt/endAt pair. We have to issue
+        // a separate query for each pair. There can be up to 9 pairs of bounds
+        // depending on overlap, but in most cases there are 4.
+        const bounds = geofire.geohashQueryBounds(center, radiusInM);
+        const promises = [];
+        for (const b of bounds) {
+            const q = await db.collection('workout')
+                .orderBy('LocationHash')
+                .startAt(b[0])
+                .endAt(b[1]);
+            promises.push(q.get());
+        }
+
+        // Collect all the query results together into a single list
+        Promise.all(promises).then((snapshots) => {
+            const matchingDocs = [];
+
+            for (const snap of snapshots) {
+                for (const doc of snap.docs) {
+                    const lat = doc.get('Latitude');
+                    const lng = doc.get('Longitude');
+
+                    // We have to filter out a few false positives due to GeoHash
+                    // accuracy, but most will match
+                    const distanceInKm = geofire.distanceBetween([parseInt(lat), parseInt(lng)], center);
+                    const distanceInM = distanceInKm * 1000;
+                    if (distanceInM <= radiusInM) {
+                        matchingDocs.push(doc);
+                    }
+                }
+            }
+            return matchingDocs;
+        }).then((matchingDocs) => {
+        // Process the matching documents
+            console.log("docs")
+            console.log(matchingDocs[0].get('Description'))
+        });
+    }
+    async function search(type, intensity, calories, lat, lng, dist){
+        // query document snapshot array
+        const filteredDocs = []
+        if (lat != null && lng !=null){
+            const matchingDocs = queryHashes(lat, lng, dist)
+            console.log(matchingDocs)
+            matchingDocs.forEach(doc =>{
+                if (doc.Category === type && doc.Intensity === intensity && doc.Calories_Burned >= calories){
+                    filteredDocs.push(doc);
+                }
+            })
+        }
+        else{
+            const filteredDocs = await db.collections['workout']
+            .where('Category', '==', type)
+            .where('Intensity', '==', intensity).get();
+        }
+        console.log(filteredDocs)
+        return filteredDocs
+    }
 
     async function handleSubmit(e){
         e.preventDefault()
@@ -86,8 +100,8 @@ export default function UploadWorkout(props) {
             setLoading(false)
             return
         }
-
-
+        setSearchResults(search(category, intensity, caloriesBurned, latitude, longitude, searchRadius))
+        console.log(searchResults[0].Name)
         setLoading(false)
     }
 
@@ -100,7 +114,7 @@ export default function UploadWorkout(props) {
 
            <div id="select-container">
                <label id="category-question" for="selectCategory">Select the category for your workout</label>
-               <select name="selectCategoty" id="category-input" value={category} onChange={(e) =>setCategory(e.target.value)}>
+               <select name="selectCategory" id="category-input" value={category} onChange={(e) =>setCategory(e.target.value)}>
                    <option value="chest">Chest</option>
                    <option value="back">Back</option>
                    <option value="bicep">Bicep</option>
@@ -154,16 +168,16 @@ export default function UploadWorkout(props) {
                 <input
                 className='inputBox'
                 type="text"
-                value={longitude}
-                onChange={(e) => setLongitude(e.target.value)}
+                value={searchRadius}
+                onChange={(e) => setSearchRadius(e.target.value)}
                 />
             </div>
         </div>
 
         <div id="submit-container">
-            {loading? <h1>Uploading your workout, please wait!</h1> : 
+            {loading? <h1>Searching, please wait!</h1> : 
             <button id='SubmitButton' onClick={handleSubmit} >
-                Submit Workout
+                Search
             </button>
             }
         </div>
