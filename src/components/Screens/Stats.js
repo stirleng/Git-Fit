21789490076dbcb0a846aa013252f96a9d4b1ef7
lzaydes <98509255+lzaydes@users.noreply.github.com>
2022-03-09 17:54:34 -0800

    Style.js changes
    Styled and implemented most of the read-script for Stats.js. Also began implementing an option for users to change info, though it may get scrapped.

--- src/components/Screens/Stats.js
+++ src/components/Screens/Stats.js
@@ -2,47 +2,29 @@ import React,  {useState, useEffect} from 'react'
 import firebase from 'firebase/compat/app';
 import { useAuth } from '../../contexts/AuthContext'
 import { db, collection, getDocs, query, where, limit, useFirestoreDocument, QuerySnapshot} from '../../firebase';
-import './Stats.css'
+import './stats.css'
 import { Link, useNavigate, useParams, useLocation} from 'react-router-dom';
 import { doc, getDoc, DocumentSnapshot, FieldPath, documentId, forEach } from "firebase/firestore";
-
- function getUserData(uid) {
-        firebase.database().ref('users/' + uid).once("Age", snap => {
-            console.log(snap.val())
-        })
-    }
+import {set, ref} from 'firebase/database'
 
 export default function Stats() {
+
+    const [userData, setUserData] = useState([])
+    const [cal, setCal] = useState(0)
+    const [proj, setProj] = useState(0)
+    const [protein, setProtein] = useState(0)
     const {currentUser} = useAuth()
-   // await db.collection("users").doc(currentUser.uid).get().then((snapshot) =>{
-        //access data
-    //})
-    let uid = currentUser.uid
-
-    let userData = []
-
-    //access user info in the backend
-        async function onRender(){
-            await db.collection("users").doc(uid).get().then((snapshot) =>{
-                        userData.push(snapshot.get("Age"))
-                         userData.push(snapshot.get("Height_ft"))
-                       userData.push(snapshot.get("Height_in"))
-                        userData.push(snapshot.get("Weight"))
-                        //userData.push(snapshot.get("Sex"))
-                  console.log(userData)
-              })
-            }
-    
-     onRender()
-
-     //doesn't update the array (?)
-    console.log(userData)
-    let age = userData[0]//getUserData(uid)
-    let sex = 'm'//currentUser.sex
-    let weight = 0.453592 * userData[3]
-    let height = 2.54 * (12 * userData[1] + userData[2])
-
+    const [name, setName] = useState('')
     const [userSeconds, setUserSeconds] = useState(0)
+    const [activeDays, setActiveDays] = useState(0)
+    const [feet, setFeet] = useState(0)
+    const [inches, setInches] = useState(0)
+    const [weight, setWeight] = useState(0)
+    const [sex, setSex] = useState('')
+    const [age, setAge] = useState(0)
+    const [toChange, setToChange] = useState('')
+    const [newVal, setNewVal] = useState(0)
+
     async function getUserSeconds(){
         await db.collection("users").doc(currentUser.uid).get().then((snapshot) =>{
             if (snapshot){
@@ -58,27 +40,112 @@ export default function Stats() {
         getUserSeconds()
       }, [])
 
-    let bmr = 0
-    if (sex =='m')
-    {
-        bmr =  88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
-    }
+   // await db.collection("users").doc(currentUser.uid).get().then((snapshot) =>{
+        //access data
+    //})
+    let uid = currentUser.uid
 
-    else{
-        bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
-    }
+    //access user info in the backend
+        async function onRender(){
+            await db.collection("users").doc(uid).get().then((snapshot) =>{
+                    setAge(snapshot.get("Age"))
+                    setFeet(snapshot.get("Height_ft"))
+                    setInches(snapshot.get("Height_in"))
+                    setWeight(snapshot.get("Weight"))
+                    setSex(snapshot.get("Sex"))
+                    let name = snapshot.get("Name")
+                    setActiveDays(snapshot.get("Chest_Days") + snapshot.get("Arms_Days") + snapshot.get("Leg_Days"))
 
-    let projected = bmr - 500
 
+                    let weight_kg = 0.453592 * weight
+                    let height_cm = 2.54 * (12 * feet + inches)
+        
+                    let bmr = 0
+                    if (sex =='m')
+                    {
+                        bmr =  1.55 * (88.362 + (13.397 * weight_kg) + (4.799 * height_cm) - (5.677 * age))
+                    }
+        
+                    else{
+                        bmr = 1.55 * (447.593 + (9.247 * weight_kg) + (3.098 * height_cm) - (4.330 * age))
+                    }
+                    setCal(bmr.toFixed(2) - 500)
+                    setProj(weight - 5)
+                    setProtein(snapshot.get("Proteins_Consumed"))
+                    setName(name)
+              })
+            }
+    
+            function changeInfo(tag, val){
+                set(ref(db, 'users/' + uid), {
+                    tag: val
+                  });
+                  onRender()
+              }
+
+        //runs whenever page loads
+         useEffect(()=>{
+             onRender()
+          }, [])
+
+          
+          let cr = (daySinceStart == 0) ? activeDays : activeDays / daySinceStart
     return(
-        <div>
-            Days since you created your account: {daySinceStart}
-            <div>
-                <button type="button" onClick={(e) => {}}>
-                    try
+        <div id='header'>
+            <h1>
+                {name}'s Fitness Numbers 
+            </h1>
+            {cal && proj && name ? <body>
+                If you eat about <b>{cal}</b> calories a day and follow your plan, you will weigh less
+                than <b>{proj}</b> lbs in just 5 weeks! <br></br>Keep it up
+                <br></br><br></br>
+                
+                It's been <b>{daySinceStart}</b> days since you joined, and you've completed <b>{activeDays}</b> workouts. 
+                <br></br><br></br>
+                Average Workouts Per Day: {cr}
+                <br></br><br></br>
+                Over the course of your time here, you've
+                consumed {protein.toFixed(2)} grams of proteins! Eating a high-protein <br></br>
+                 diet is the best way to build muscle while burning fat!
+                 <br></br><br></br>
+
+                Your Physique Info:
+                <div id='info'>
+                    {age}{sex}: {feet}'{inches}" and {weight} lbs
+                </div>
+
+            </body>: <body>
+                Loading
+            </body>}
+            <div id='dualField'>
+            <div id = "select">
+                <select onClick={(e) => {setToChange(e.target.value)}}>
+                    Workout Preference
+                    <option value=''>Need to update some info?</option>
+                    <option value = "Age">Age</option>
+                    <option value="Height_ft">Height (feet)</option>
+                    <option value="Height_in">Height (inches)</option>
+                    <option value="Weight">Weight</option>
+                </select>
+
+             <div>
+                <input 
+                    className='inputBox' 
+                    placeholder=' New Value'
+                    type="text"
+                    value={newVal}
+                    onChange={(e) =>{
+                        setNewVal(e.target.value)
+                       }
+                    }
+                  />   
+                  </div>
+
+                <button type ="button" onClick={(e) => {changeInfo(toChange, newVal)}}>
+                    Update Info    
                 </button>
             </div>
-            If you eat fewer than {age} calories a day, you will be lbs in 5 weeks!
+        </div>
         </div>
     )
 }
